7b34e24da6bd38ff037c06d5be455d071dfe2c66
C2A-2 <149025632+C2A-2@users.noreply.github.com>
2023-11-22 01:25:45 +0900

    フォーム間受け渡しから

---
+++ Weather/Weather/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.latBox = new System.Windows.Forms.TextBox();
+            this.lonBox = new System.Windows.Forms.TextBox();
+            this.maxtemCheckBox = new System.Windows.Forms.CheckBox();
+            this.mintemCheckBox = new System.Windows.Forms.CheckBox();
+            this.winspCheckBox = new System.Windows.Forms.CheckBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.rainFallParCheckBox = new System.Windows.Forms.CheckBox();
+            this.wetherCodeCheckBox = new System.Windows.Forms.CheckBox();
+            this.uvCheckBox = new System.Windows.Forms.CheckBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.timezoneComboBox = new System.Windows.Forms.ComboBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HGS明朝E", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(122, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "☺たくくんのおてんきよほう☺";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(14, 117);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "緯度";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(14, 157);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "経度";
+            // 
+            // latBox
+            // 
+            this.latBox.Location = new System.Drawing.Point(59, 114);
+            this.latBox.Margin = new System.Windows.Forms.Padding(2);
+            this.latBox.Name = "latBox";
+            this.latBox.Size = new System.Drawing.Size(129, 19);
+            this.latBox.TabIndex = 3;
+            this.latBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lonBox
+            // 
+            this.lonBox.Location = new System.Drawing.Point(59, 154);
+            this.lonBox.Margin = new System.Windows.Forms.Padding(2);
+            this.lonBox.Name = "lonBox";
+            this.lonBox.Size = new System.Drawing.Size(129, 19);
+            this.lonBox.TabIndex = 4;
+            this.lonBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // maxtemCheckBox
+            // 
+            this.maxtemCheckBox.AutoSize = true;
+            this.maxtemCheckBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.maxtemCheckBox.Location = new System.Drawing.Point(195, 113);
+            this.maxtemCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.maxtemCheckBox.Name = "maxtemCheckBox";
+            this.maxtemCheckBox.Size = new System.Drawing.Size(94, 20);
+            this.maxtemCheckBox.TabIndex = 8;
+            this.maxtemCheckBox.Text = "最高気温";
+            this.maxtemCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // mintemCheckBox
+            // 
+            this.mintemCheckBox.AutoSize = true;
+            this.mintemCheckBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.mintemCheckBox.Location = new System.Drawing.Point(195, 154);
+            this.mintemCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.mintemCheckBox.Name = "mintemCheckBox";
+            this.mintemCheckBox.Size = new System.Drawing.Size(94, 20);
+            this.mintemCheckBox.TabIndex = 9;
+            this.mintemCheckBox.Text = "最低気温";
+            this.mintemCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // winspCheckBox
+            // 
+            this.winspCheckBox.AutoSize = true;
+            this.winspCheckBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.winspCheckBox.Location = new System.Drawing.Point(291, 113);
+            this.winspCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.winspCheckBox.Name = "winspCheckBox";
+            this.winspCheckBox.Size = new System.Drawing.Size(60, 20);
+            this.winspCheckBox.TabIndex = 10;
+            this.winspCheckBox.Text = "風速";
+            this.winspCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.startButton.Location = new System.Drawing.Point(17, 187);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(185, 40);
+            this.startButton.TabIndex = 11;
+            this.startButton.Text = "実行";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.resetButton.Location = new System.Drawing.Point(232, 187);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(104, 41);
+            this.resetButton.TabIndex = 12;
+            this.resetButton.Text = "リセット";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // rainFallParCheckBox
+            // 
+            this.rainFallParCheckBox.AutoSize = true;
+            this.rainFallParCheckBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.rainFallParCheckBox.Location = new System.Drawing.Point(291, 154);
+            this.rainFallParCheckBox.Name = "rainFallParCheckBox";
+            this.rainFallParCheckBox.Size = new System.Drawing.Size(94, 20);
+            this.rainFallParCheckBox.TabIndex = 13;
+            this.rainFallParCheckBox.Text = "降水確率";
+            this.rainFallParCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // wetherCodeCheckBox
+            // 
+            this.wetherCodeCheckBox.AutoSize = true;
+            this.wetherCodeCheckBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.wetherCodeCheckBox.Location = new System.Drawing.Point(392, 154);
+            this.wetherCodeCheckBox.Name = "wetherCodeCheckBox";
+            this.wetherCodeCheckBox.Size = new System.Drawing.Size(60, 20);
+            this.wetherCodeCheckBox.TabIndex = 14;
+            this.wetherCodeCheckBox.Text = "天気";
+            this.wetherCodeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // uvCheckBox
+            // 
+            this.uvCheckBox.AutoSize = true;
+            this.uvCheckBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.uvCheckBox.Location = new System.Drawing.Point(392, 116);
+            this.uvCheckBox.Name = "uvCheckBox";
+            this.uvCheckBox.Size = new System.Drawing.Size(77, 20);
+            this.uvCheckBox.TabIndex = 15;
+            this.uvCheckBox.Text = "紫外線";
+            this.uvCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.exitButton.Location = new System.Drawing.Point(368, 188);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(101, 40);
+            this.exitButton.TabIndex = 16;
+            this.exitButton.Text = "終了";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // timezoneComboBox
+            // 
+            this.timezoneComboBox.FormattingEnabled = true;
+            this.timezoneComboBox.Location = new System.Drawing.Point(331, 72);
+            this.timezoneComboBox.Name = "timezoneComboBox";
+            this.timezoneComboBox.Size = new System.Drawing.Size(121, 20);
+            this.timezoneComboBox.TabIndex = 17;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 333);
+            this.Controls.Add(this.timezoneComboBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.uvCheckBox);
+            this.Controls.Add(this.wetherCodeCheckBox);
+            this.Controls.Add(this.rainFallParCheckBox);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.winspCheckBox);
+            this.Controls.Add(this.mintemCheckBox);
+            this.Controls.Add(this.maxtemCheckBox);
+            this.Controls.Add(this.lonBox);
+            this.Controls.Add(this.latBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "天気予報";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox latBox;
+        private System.Windows.Forms.TextBox lonBox;
+        private System.Windows.Forms.CheckBox maxtemCheckBox;
+        private System.Windows.Forms.CheckBox mintemCheckBox;
+        private System.Windows.Forms.CheckBox winspCheckBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.CheckBox rainFallParCheckBox;
+        private System.Windows.Forms.CheckBox wetherCodeCheckBox;
+        private System.Windows.Forms.CheckBox uvCheckBox;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ComboBox timezoneComboBox;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ Weather/Weather/Form1.cs
@@ -0,0 +1,180 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weather
+{
+    public partial class Form1 : Form
+    {
+
+        float lat;
+        float lon;
+        float maxtem;
+        float mintem;
+        float winsp;
+        string URL;
+        Stream responseStream;
+        Form2 f2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //実行ボタンクリック
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            parameters();
+            generationURL();
+            f2.ShowDialog();
+        }
+
+        //入力された緯度経度取得
+        public void parameters()
+        {
+            lat = float.Parse(latBox.Text);
+            lon = float.Parse(lonBox.Text);
+           
+        }
+
+        //URLの生成
+        public void generationURL()
+        {
+            URL = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon;
+
+            //チェックボックスが1つ以上選択されているかどうか
+            if(maxtemCheckBox.Checked == true|| mintemCheckBox.Checked == true|| winspCheckBox.Checked == true)
+            {
+                URL = URL + "&daily=";
+
+                //最高気温チェックボックスチェック
+                if(maxtemCheckBox.Checked == true)
+                {
+                    URL = URL + "temperature_2m_max,";
+                }
+
+                //最低気温チェックボックスチェック
+                if(mintemCheckBox.Checked == true)
+                {
+                    URL = URL + "temperature_2m_min,";
+                }
+
+                //風速チェックボックスチェック
+                if(winspCheckBox.Checked == true)
+                {
+                    URL = URL + "wind_speed_10m_max,";
+                }
+
+                //降水確率チェックボックスチェック
+                if (rainFallParCheckBox.Checked == true)
+                {
+                    URL = URL + "precipitation_probability_max,";
+                }
+
+                //天気コードチェックボックスチェック
+                if (wetherCodeCheckBox.Checked == true)
+                {
+                    URL = URL + "weather_code,";
+                }
+
+                //紫外線チェックボックスチェック
+                if (uvCheckBox.Checked == true)
+                {
+                    URL = URL + "uv_index_max,";
+                }
+            }
+
+            //URL最後尾のコンマの削除
+            URL = URL.TrimEnd(',');
+            //Console.WriteLine(URL);
+            //Console.ReadLine();
+
+            //タイムゾーン設定、東京固定
+            URL = URL + "&timezone=Asia%2FTokyo";
+
+
+        }
+
+        //URLの取得
+        public void accessURL()
+        {
+            WebRequest request = WebRequest.Create(URL);
+            responseStream = request.GetResponse().GetResponseStream();
+        }
+
+        //天気情報取得、表示
+        public void getInformationView()
+        {
+            StreamReader reader = new StreamReader(responseStream);
+            var obj_from_json = JObject.Parse(reader.ReadToEnd());
+
+            var getTimezone = obj_from_json["timezone"];
+            if (maxtemCheckBox.Checked == true)
+            {
+
+                var forecast_timezone_abbreviation = obj_from_json["timezone_abbreviation"];
+                var forecast_latitude = obj_from_json["latitude"];
+                var forecast_longitude = obj_from_json["longitude"];
+                var forecast_hourly_units = obj_from_json["hourly_units"]["temperature_2m"];
+            }
+
+            if (mintemCheckBox.Checked == true)
+            {
+                var forecast_timezone = obj_from_json["timezone"];
+                var forecast_timezone_abbreviation = obj_from_json["timezone_abbreviation"];
+                var forecast_latitude = obj_from_json["latitude"];
+                var forecast_longitude = obj_from_json["longitude"];
+                var forecast_hourly_units = obj_from_json["hourly_units"]["temperature_2m"];
+            }
+
+            if (winspCheckBox.Checked == true)
+            {
+                var forecast_timezone = obj_from_json["timezone"];
+                var forecast_timezone_abbreviation = obj_from_json["timezone_abbreviation"];
+                var forecast_latitude = obj_from_json["latitude"];
+                var forecast_longitude = obj_from_json["longitude"];
+                var forecast_hourly_units = obj_from_json["hourly_units"]["temperature_2m"];
+            }
+        }
+
+        //入力項目全リセット
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            latBox.Text = "";
+            lonBox.Text = "";
+            maxtemCheckBox.Checked = false;
+            mintemCheckBox.Checked = false;
+            winspCheckBox.Checked = false;
+            rainFallParCheckBox.Checked = false;
+            wetherCodeCheckBox.Checked = false;
+            uvCheckBox.Checked = false;
+        }
+
+        //プログラム終了
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            f2 = new Form2();
+        }
+    }
+
+            
+}
